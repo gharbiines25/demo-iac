1c8fe8c408aaa22a3cc4de122d67d6f7b73c1549
gharbiines25 <gharbiines2021@gmail.com>
2025-05-07 11:57:36 +0200

    Create  main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  endpoints {
+    ec2 = "http://ip10-0-20-5-d0dh37g05akh4glkf9hg-4566.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
